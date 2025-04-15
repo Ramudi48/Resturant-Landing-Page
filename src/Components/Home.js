17e381e0f56372dc258dc1bc0658782a19f8d663
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-04-15 22:05:33 +0530

    Fix formatting in Home component by removing unnecessary whitespace

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,9 @@ const Home = () => {
     <div className="home-container">
       <Navbar />
       <div className="home-banner-container">
-        
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
         <div className="home-text-section">
           <h1 className="primary-heading">
             Your Favourite Food Delivered Hot & Fresh
